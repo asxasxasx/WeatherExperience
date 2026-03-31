c41250568a0c6ab289a8d4a1e1a7fc9ed7b63624
Aleksandr Chasovskikh <a.chasovskikh@gmail.com>
2026-03-31 22:04:41 +0700

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,25 @@
+//  
+//  ContentView.swift
+//  Weather
+//
+//  Created by Alexander Chasovskikh on 31.03.2026.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,18 @@
+//  
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Alexander Chasovskikh on 31.03.2026.
+//  
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
